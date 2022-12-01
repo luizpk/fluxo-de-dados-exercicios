0e5e7fea4aa548cf745c5acbd6a310a3509ab682
LuizPK <luizpkpinheiro@gmail.com>
2022-12-01 17:22:19 -0300

    Resolvendo exercícios de fluxo

--- src/components/FormularioLogin/FormularioLogin.js
+++ src/components/FormularioLogin/FormularioLogin.js
@@ -1,3 +1,4 @@
+import { getValue } from "@testing-library/user-event/dist/utils";
 import React from "react";
 import {
   Form,
@@ -8,19 +9,31 @@ import {
 } from "../FormularioPostagem/styled";
 
 const FormularioLogin = (props) => {
+
+  const onChangeNome = (event)=>{
+    props.setNome(event.target.value)
+  }
+  const onChangeFoto=(event)=>{
+    props.setFoto(event.target.value)
+  
+  }
+
   const login = () => {
     props.setPageFlow(2);
   };
+
+
+
   return (
     <FormContainer>
       <Form>
         <StyledLabel>
           Nome:
-          <Input type={"text"} />
+          <Input type={"text"} value={props.nome} onChange={onChangeNome} />
         </StyledLabel>
         <StyledLabel>
           Foto de Perfil
-          <Input type={"text"} />
+          <Input type={"text"} value={props.foto} onChange={onChangeFoto} />
         </StyledLabel>
         <SendButton onClick={login}>Fazer Login</SendButton>
       </Form>
